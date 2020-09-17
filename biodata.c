8a0f9d06ec422944aecc099ceeeaa2ecf631e621
Joni Bastian <jonibastian01@gmail.com>
2020-09-18 01:21:35 +0700

    Menambahkan file biodta.c

---
+++ biodata.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main() {
+	
+	printf("====Biodata===\n\n");
+	
+	printf("Nama : Joni Bastian");
+	printf("\n");
+	printf("Nama Panggilan : Jobas");
+	printf("\n");
+	printf("Gol. Darah : O");
+	printf("\n");
+	printf("Pekerjaan : Mahasiswa");
+	printf("\n");
+	printf("Institusi : UPN Veteran Jawa Timur");
+	printf("\n");
+	printf("Alamat : Jl. Sunaryo km. 04 RT.01 RW.03 Kel. Tg. Batu Barat, Kec. Kundur, Kab. Karimun, Kep. Riau, 29662");
+	printf("\n");
+	printf("Hobi : Olahraga, musik");
+	
+	return 0;
+}
